d9d741713892b2cddb10b1263158484bc508b5de
kobbootcamp <kobbootcamp@gmail.com>
2018-11-18 18:54:38 -0600

    MVP ready

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,6 @@
 
-$(document).ready(function () {
 
 
-    $(".pic-container").on("click", function () {
-
-        switch (this.id) {
-            case "word":
-                window.open("https://kobbootcamp.github.io/Word-Guess-Game/");
-                break;
-            case "starwars":
-                window.open("https://kobbootcamp.github.io/unit-4-game/");
-                break;
-            case "trivia":
-                window.open("https://kobbootcamp.github.io/TriviaGame/");
-                break;
-            case "farm":
-                window.open("https://kobbootcamp.github.io/GifTastic/");
-                break;
-            case "train":
-                window.open("https://kobbootcamp.github.io/TrainScheduler/");
-                break;
-        }
-    })
-
-
-})
+function OpenLinkedIn() {
+    window.open("https://www.linkedin.com/in/kevin-o-brien-a1416222/");
+}
